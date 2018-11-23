6dcfd58f7e651ce20e488be269a99e7f2186c9b3
Iacopo Ghinassi <45108036+Ighina@users.noreply.github.com>
2018-11-23 18:17:33 +0000

    Update app.R

--- SemanticApp/app.R
+++ SemanticApp/app.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("shinytheme", "shiny", "igraph", "tm", "tokenizers","stringr","readr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(shiny)
 library(shinythemes)
 
